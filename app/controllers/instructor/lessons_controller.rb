d6b32d4f994c923829294736547b362c93fe8c6a
adamjgoodman <goodmanadamj@gmail.com>
2019-10-16 02:07:45 +0000

    upload image for course

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -6,18 +6,18 @@ class Instructor::LessonsController < ApplicationController
 		@lesson = Lesson.new
 	end
 
-	def create
-		@lesson = current_section.lessons.create(lesson_params)
-		redirect_to instructor_course_path(@section.course)
-	end
-
+  def create
+    @lesson = current_section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_section.course)
+  end
+  
 	private
 
-	def require_authorized_for_current_section
-		if current_section.course.user != current_user
-			return render plan: 'Unauthorized', status :unauthorized 
-		end
-	end
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
 
 	helper_method :current_section
 	def current_section
