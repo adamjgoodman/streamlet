9d0ee1872bb728993f0d6046cd6140fdf9870f56
adamjgoodman <goodmanadamj@gmail.com>
2019-10-22 21:20:54 +0000

    lesson reorder javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,8 @@
 //= require turbolinks
 //= require popper
 //= require tether
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+//= require jquery-ui
