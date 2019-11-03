ae764b768f831ca7ce0910b481bd01aba6f3e872
adamjgoodman <goodmanadamj@gmail.com>
2019-11-03 03:09:30 +0000

    restrict lessons to enrolled users

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+	before_action :require_enrollment_for_current_user, only: [:show]
+
 	def show
+
 	end
 
-	private 
+	private
+	
+	def require_enrollment_for_current_user
+		 if current_user.enrolled_in?(current_lesson.section.course) == false
+      redirect_to course_path(current_lesson.section.course), alert: 'Must be enrolled to view lessons'
+    end
+	end 
 
 	helper_method :current_lesson
 	def current_lesson
